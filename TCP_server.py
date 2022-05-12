b7e425af0100594e0dc2227f7f48246b92ce0e3a
MahatKC <mkarvat@gmail.com>
2022-05-12 14:42:53 -0300

    tcp server function

--- TCP_server.py
+++ TCP_server.py
@@ -1,26 +1,22 @@
-# echo-server.py
-
 import socket
 import time
 
 HOST = "123.123.123.123"  # Standard loopback interface address (localhost)
 PORT = 54321  # Port to listen on (non-privileged ports are > 1023)
 
-f = open('arquivo_recebido.txt', 'wb')
-i=0
+def TCP_server(tamanho, HEADER_SIZE):
+    f = open('arquivo_recebido.txt', 'wb')
+    t0 = time.time()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        l = conn.recv(tamanho - HEADER_SIZE)
+        while(l):
+            f.write(l)
+            l = conn.recv(tamanho - HEADER_SIZE)
+        f.close()
+        conn.close()
+    t1 = time.time()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    l = conn.recv(946)
-    while(l):
-        print(l)
-        f.write(l)
-        l = conn.recv(946)
-        time.sleep(0.1)
-        i+=1
-        if i==10:
-            break
-    f.close()
-    conn.close()
+    return t1-t0
