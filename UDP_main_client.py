b5724c924234efd993bdc10648c7129bca050a06
robertaalcantara <roberta_alcantara@outlook.com.br>
2022-05-13 15:38:43 -0300

    contador pacotes

--- UDP_main_client.py
+++ UDP_main_client.py
@@ -3,24 +3,27 @@ import time
 import pandas as pd
 
 tamanhos = [100, 500, 1000]
-HEADER_SIZE = 0
+HEADER_SIZE = 42
 
 for tamanho in tamanhos:
     tempos = []
     sizes = []
+    total_pacotes = []
+
     for i in range(10):
         #time.sleep(0.5)
-        tempo = UDP_client(tamanho, HEADER_SIZE)
+        tempo, contador_pacotes = UDP_client(tamanho, HEADER_SIZE)
+        close_UDP()
         tempos.append(tempo)
         sizes.append(tamanho)
+        total_pacotes.append(contador_pacotes)
         print(f"Finished iteration {i} for size {tamanho}")
-    close_UDP(tamanho, HEADER_SIZE)
-             
-    #time.sleep(0.5)
+        #time.sleep(0.5)
 
     results = pd.DataFrame({
             'Tamanho': sizes,
-            'Tempos': tempos
+            'Tempos': tempos,
+            'Pacotes': total_pacotes
         })
     if tamanho==100:
         results.to_csv("ResultadosUDPSemGarantia.csv",index=False)
