71c64ceb0cdbc25e41644a4f99e0392b4d908c4c
MahatKC <mkarvat@gmail.com>
2022-05-13 15:46:20 -0300

    udp

--- UDP_server.py
+++ UDP_server.py
@@ -1,7 +1,7 @@
 import socket
 
-localIP     = "123.123.123.123"
-localPort   = 55443
+localIP = "123.123.123.123"
+localPort = 55443
 
 def UDP_server(tamanho, HEADER_SIZE):
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -13,11 +13,11 @@ def UDP_server(tamanho, HEADER_SIZE):
 
     while(udp_open):
         bytesAddressPair = s.recvfrom(tamanho - HEADER_SIZE)
+        message = bytesAddressPair[0]
 
-        if message=='':
+        if message.decode('utf-8') == 'CRm0W>W?;GQ4AP.sSg':
             udp_open = False
         else:
-            message = bytesAddressPair[0]
             f.write(message)
-            
+
     f.close()
