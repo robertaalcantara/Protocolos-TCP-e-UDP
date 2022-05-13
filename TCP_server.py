af69c2bc78daa26d2ef36510ffc8ae05294d7a30
MahatKC <mkarvat@gmail.com>
2022-05-13 14:11:54 -0300

    server updates

--- TCP_server.py
+++ TCP_server.py
@@ -1,9 +1,10 @@
 import socket
+import time
 
 HOST = "123.123.123.123"  # Standard loopback interface address (localhost)
-PORT = 54321  # Port to listen on (non-privileged ports are > 1023)
+PORT = 55443  # Port to listen on (non-privileged ports are > 1023)
 
-def TCP_server(tamanho, HEADER_SIZE):
+def TCP_server(tamanho, HEADER_SIZE, extra_sleep):
     f = open('arquivo_recebido.txt', 'wb')
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,5 +16,8 @@ def TCP_server(tamanho, HEADER_SIZE):
         while(l):
             f.write(l)
             l = conn.recv(tamanho - HEADER_SIZE)
+        if extra_sleep:
+            time.sleep(2)
+        time.sleep(0.1)
         f.close()
         conn.close()
--- main_server.py
+++ main_server.py
@@ -1,8 +1,12 @@
 from TCP_server import TCP_server 
+import time
 
 tamanhos = [100, 500, 1000]
 HEADER_SIZE = 54
 
 for tamanho in tamanhos:
     for i in range(10):
-        TCP_server(tamanho, HEADER_SIZE)
+        TCP_server(tamanho, HEADER_SIZE, (i==9 and tamanho==1000))
+        print(f"Finished iteration {i} for size {tamanho}")
+        time.sleep(0.1)
+    time.sleep(0.3)
