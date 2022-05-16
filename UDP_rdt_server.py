4a7d87afee351b5531804ea8afa04e282ade68e6
MahatKC <mkarvat@gmail.com>
2022-05-16 15:40:40 -0300

    rdt server

--- UDP_rdt_server.py
+++ UDP_rdt_server.py
@@ -1,4 +1,5 @@
 import socket
+from tabnanny import check
 
 localIP = "123.123.123.123"
 localPort = 55443
@@ -7,16 +8,35 @@ def verify_checksum(message):
     binary_string = "".join(f"{ord(i):08b}" for i in message.decode('utf-8'))
     word_size_in_bits = (len(message)//4)*8
 
-    binary_string = binary_string.zfill(word_size_in_bits*3)
+    binary_string = binary_string.zfill(word_size_in_bits*4)
 
-    c1 = binary_string[0:word_size_in_bits]
+    c1 = binary_string[:word_size_in_bits]
     c2 = binary_string[word_size_in_bits:word_size_in_bits*2]
     c3 = binary_string[word_size_in_bits*2:word_size_in_bits*3]
-    c4 = binary_string[word_size_in_bits*3:word_size_in_bits*4]
 
-    sum = bin(int(c1,2)+int(c2,2)+int(c3,2)+int(c4,2))[2:]
+    sum = bin(int(c1,2)+int(c2,2)+int(c3,2))[2:]
 
-    for char in sum:
+    if(len(sum)>word_size_in_bits):
+        x = len(sum)-word_size_in_bits
+        sum = bin(int(sum[:x], 2) + int(sum[x:],2))[2:]
+    if(len(sum)<word_size_in_bits):
+        sum = '0'*(word_size_in_bits-len(sum))+sum
+
+    checksum = binary_string[word_size_in_bits*3:word_size_in_bits*4]
+    checksons = binary_string[word_size_in_bits*4:]
+    
+    for i in range(len(checksons[1:])):
+        if i == 8:
+            continue
+        elif checksons[i] == '1':
+            if i < 8:
+                checksum = checksum[:(i-1)*8]+'1'+checksum[((i-1)*8)+1:]
+            else:
+                checksum = checksum[:(i-2)*8]+'1'+checksum[((i-2)*8)+1:]
+
+    final_sum = bin(int(sum,2)+int(checksum,2))[2:]
+
+    for char in final_sum:
         if char == '0':
             return False, ''
     
@@ -36,12 +56,16 @@ def UDP_rdt_server(tamanho, HEADER_SIZE):
 
         if message.decode('utf-8') == 'CRm0W>W?;GQ4AP.sSg':
             udp_open = False
+        elif len(message.decode('utf-8')) != ((tamanho - HEADER_SIZE)*3)//4:
+            s.sendto(str.encode('ACK'), bytesAddressPair[1])
+            f.write(message)
         else:
             is_ack, message_content = verify_checksum(message)
             if is_ack:
                 s.sendto(str.encode('ACK'), bytesAddressPair[1])
                 f.write(message_content)
             else:
+                print("NACK")
                 s.sendto(str.encode('NACK'), bytesAddressPair[1])
 
     f.close()
